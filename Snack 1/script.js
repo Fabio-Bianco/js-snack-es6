3feeb10695a91a9d8f318c9f20b2c03401256db5
Fabio-Bianco <biancobfabio@gmail.com>
2025-03-08 14:28:57 +0100

    Snack 1 Creare un array di oggetti: solved

--- Snack 1/script.js
+++ Snack 1/script.js
@@ -5,51 +5,6 @@
 // Stampare a schermo la bici con peso minore. -->
 
 
-// Array di oggetti 
-
-// const bike = [
-//   {
-//     nome: "Bianchi Oltre",
-//     peso: 7.2,
-//   },
-
-//   {
-//     nome: "Pinarello Dogma",
-//     peso: 6.8,
-//   },
-
-//   {
-//     nome: "Colnago V3Rs",
-//     peso: 7.0,
-//   },
-
-//   { nome: "Trek Emonda",
-//      peso: 6.5 
-//   },
-
-//   { nome: "Specialized Tarmac", 
-//     peso: 6.7 
-//   },
-// ];
-
-// // Stampo l'intero array
-// console.log (bike);
-
-// // stampo in consolle la bici con il peso minore (oggetto)
-// console.log (bike[3]);
-
-
-// // Con un ciclo for sull'arry trovo la bici con il peso minore;
-
-// let lightBike = bike [0];
-
-// for (let i = 1; i = bike.length; i++ ) {
-//      if (bike[i].peso < lightBike.peso) {
-//         lightBike = bike[i];
-//      }
-     
-// }
-
 // Creazione dell'array di oggetti con nome e peso delle bici
 const biciclette = [
     { nome: "Bianchi Oltre", peso: 7.2 },
